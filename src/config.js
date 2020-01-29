dd94f59ce3d2e67c13bba778b11aadad93c937e7
kyawthura <kyawthura@immin.io>
2020-01-29 12:37:06 +0800

    adding production file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://127.0.0.1:27018/test';
+const url = 'mongodb://mongos1:27017,mongos2:27017/akwebdb';
 
 mongoose.connect(url, { useNewUrlParser: true });
 mongoose.connection.once('open', () =>
